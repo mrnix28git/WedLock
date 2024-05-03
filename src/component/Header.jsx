66448ca9e60603fd94762f9f07dae7c3998d8cb9
mrnix28pro <133763274+mrnix28pro@users.noreply.github.com>
2024-05-03 12:13:38 +0530

    full responsive

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -4,23 +4,23 @@ import Lan from '../assets/Aus.svg'
 import { RiArrowDropDownLine } from "react-icons/ri";
 const Header = () => {
     return (
-        <div className="flex items-center justify-between px-10 bg-black">
+        <div className="flex items-center justify-between md:px-10 px-5 bg-black">
             <div className=' text-white'>
-                <ul className='flex gap-5 items-center'>
+                <ul className='hidden md:flex gap-5 items-center'>
                     <li>Mission</li>
                     <li>Advice</li>
                     <li>Help</li>
                 </ul>
             </div>
-            <div className=''>
+            <div className='w-40 md:w-auto'>
                 <img src={Logo} alt="" />
             </div>
             <div className='flex gap-5'>
-                <button className='flex items-center  justify-center  gap-3 text-white rounded-3xl border-white border-2 w-32 text-xl'>
+                <button className='flex items-center  justify-center  md:gap-3 text-white rounded-3xl border-white border-2 md:w-32 w-20 md:text-xl p-1'>
                     Login
-                    <RiArrowDropDownLine  className="text-3xl"/>
+                    <RiArrowDropDownLine className="text-3xl" />
                 </button>
-                <img src={Lan} alt="" className=''/>
+                <img src={Lan} alt="" className='w-10' />
             </div>
 
         </div>
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -67,7 +67,7 @@ const Home = () => {
             {/* main end */}
             {/* archive */}
 
-            <div className='bg-[#009BDA] text-white grid grid-cols-3 '>
+            <div className='bg-[#009BDA] text-white md:grid grid-cols-3 '>
                 <div className='flex flex-col items-center gap-4 text-center hover:bg-[#007EAF] py-6'>
                     <CiHeart className='text-4xl' />
                     <h1 className='text-4xl font-bold'>1000+</h1>
@@ -91,24 +91,24 @@ const Home = () => {
 
             {/* trust  */}
 
-            <div className='space-y-16 py-32 pl-20'>
+            <div className='md:space-y-16 space-y-8 md:py-32 md:pl-20 p-10'>
                 <h1 className='text-[#231F20B2] text-2xl'>Trust</h1>
-                <div className='space-y-10'>
-                    <h1 className='text-[#007EAF] text-6xl font-bold'>The premier dating application with unparalleled trust.
+                <div className='md:space-y-10 space-y-5'>
+                    <h1 className='text-[#007EAF] md:text-6xl text-3xl font-bold'>The premier dating application with unparalleled trust.
                     </h1>
                     <p className='text-[#231F20B2] text-2xl'>With over two decades of reliable matchmaking experience,<br /> supported by a committed Trust and Safety team at your service.
                     </p>
                 </div>
-                <button className='text-white bg-[#009BDA] rounded-full w-64 h-24 text-3xl font-semibold'>Join Now</button>
+                <button className='text-white bg-[#009BDA] rounded-full md:w-64 md:h-24 md:text-3xl w-32 h-10 font-semibold'>Join Now</button>
             </div>
 
             {/* trust end */}
 
             {/* watch  */}
 
-            <div className='bg-[#007EAF] text-white px-20 py-20 space-y-14'>
-                <h1 className='font-Glory text-4xl'>Watch us</h1>
-                <h1 className='text-7xl font-bold'>Introducing: A new eharmony experience
+            <div className='bg-[#007EAF] text-white md:px-20 md:py-20 md:space-y-14 p-8 space-y-8'>
+                <h1 className='font-Glory md:text-4xl text-2xl'>Watch us</h1>
+                <h1 className='md:text-7xl text-3xl font-bold'>Introducing: A new eharmony experience
                 </h1>
                 <p className='text-xl'>Your search for a great relationship has never been easier with <br /> groundbreaking overhaul of the eharmony you know and trust.
                 </p>
@@ -119,20 +119,20 @@ const Home = () => {
 
             {/* stories */}
 
-            <div className='space-y-16 px-20 py-20'>
-                <h1 className='text-[#231F20E5] text-2xl'>Success stories</h1>
-                <div className='space-y-10'>
-                    <div className='flex items-center gap-96'>
-                        <h1 className='text-[#007EAF] text-6xl font-bold'>Lovely Happy Stories</h1>
+            <div className='md:space-y-16 md:px-20 md:py-20 space-y-5 p-8'>
+                <h1 className='text-[#231F20E5] md:text-2xl text-xl'>Success stories</h1>
+                <div className='md:space-y-10 space-y-5'>
+                    <div className='flex items-center md:gap-96 gap-20'>
+                        <h1 className='text-[#007EAF] md:text-6xl text-3xl font-bold'>Lovely Happy Stories</h1>
                         <div className='flex gap-4'>
-                            <IoArrowBack className='text-[#231F20E5] w-16 h-16' />
-                            <IoArrowForward className=' text-white rounded-full bg-[#009BDA] w-16 h-16' />
+                            <IoArrowBack className='text-[#231F20E5] md:w-16 md:h-16' />
+                            <IoArrowForward className=' text-white rounded-full bg-[#009BDA] md:w-16 md:h-16' />
 
                         </div>
                     </div>
-                    <p className='text-[#231F20E5] text-2xl'>Dive into stories of unexpected friendships, love that blossoms in the most extraordinary places, and dreams that come true against all odds.</p>
+                    <p className='text-[#231F20E5] md:text-2xl text-xl'>Dive into stories of unexpected friendships, love that blossoms in the most extraordinary places, and dreams that come true against all odds.</p>
                 </div>
-                <div className='flex gap-5'>
+                <div className='flex gap-5 overflow-x-scroll' >
                     {/* <div className='relative'>
                         <img src={Story} alt="" className='rounded-3xl w-72' />
                         {/* <div className=' absolute bottom-0 '>
@@ -156,7 +156,7 @@ const Home = () => {
 
             {/* feature */}
 
-            <div className='bg-[#007EAF] flex text-white p-20 '>
+            <div className='bg-[#007EAF] flex flex-col md:flex-row gap-8 text-white md:p-20 p-8  '>
                 <div className='space-y-12'>
                     <h1 className='text-2xl'>Features</h1>
                     <h1 className='text-5xl font-bold'>Connect with matches the way you like
@@ -174,17 +174,18 @@ const Home = () => {
             {/* feature end */}
             {/* Ai feature  */}
 
-            <div className='bg-[#007EAF] space-y-40 px-20 pt-20' >
+            <div className='bg-[#007EAF] md:space-y-40 md:px-20 md:pt-20 p-10 ' >
                 <div className='relative text-white space-y-10'>
-                    <h1 className='text-2xl'>AI Features</h1>
-                    <p className='text-5xl font-bold'>AI-driven match <br /> recommendations to suit your <br /> preferences
+                    <h1 className='md:text-2xl text-xl'>AI Features</h1>
+                    <p className='md:text-5xl text-2xl font-bold'>AI-driven match <br /> recommendations to suit your <br /> preferences
                     </p>
-                    <img src={Thunder} alt="" className='absolute top-0 right-0' />
+                    <img src={Thunder} alt="" className='absolute md:top-0 -top-10  right-0 w-16 md:w-auto
+                    ' />
                 </div>
                 <div className=''>
-                    <div className='bg-[#D3D3D380] border-[#FFFFFF] rounded-t-3xl w-[50rem] h-[34.5rem] relative left-52'>
+                    <div className='bg-[#D3D3D380] border-[#FFFFFF] rounded-t-3xl md:w-[50rem] md:h-[34.5rem] h-52 relative md:left-52 -bottom-10 '>
                         <div>
-                            <img src={Ai} alt="" className='relative left-40 -top-10' />
+                            <img src={Ai} alt="" className='relative md:left-40 left-20 md:-top-10 -top-5 w-52  md:w-auto ' />
                         </div>
                     </div>
                 </div>
@@ -195,10 +196,10 @@ const Home = () => {
 
             {/* About */}
 
-            <div className='space-y-20 p-20'>
-                <h1 className='text-[#231F20E5] text-3xl font-Glory'>About</h1>
-                <h1 className='text-[#007EAF] text-5xl font-bold'>About Wedlock</h1>
-                <p className='text-[#231F20E5] text-2xl'>Wedlock, a trailblazer in online matchmaking, is a trusted resource for millions of Indians seeking lifelong partners. Recognized as the most reliable online matrimony service by the Brand Trust Report, Wedlock also holds a record in the Limca Book of Records for the most documented marriages facilitated online. Our mission is to strengthen India by fostering happy and fulfilling unions.
+            <div className='md:space-y-20 md:p-20 space-y-8 p-8'>
+                <h1 className='text-[#231F20E5] md:text-3xl text-xl font-Glory'>About</h1>
+                <h1 className='text-[#007EAF] md:text-5xl text-2xl font-bold'>About Wedlock</h1>
+                <p className='text-[#231F20E5] md:text-2xl text-xl'>Wedlock, a trailblazer in online matchmaking, is a trusted resource for millions of Indians seeking lifelong partners. Recognized as the most reliable online matrimony service by the Brand Trust Report, Wedlock also holds a record in the Limca Book of Records for the most documented marriages facilitated online. Our mission is to strengthen India by fostering happy and fulfilling unions.
                 </p>
 
             </div>
@@ -208,9 +209,9 @@ const Home = () => {
 
             {/* watch  */}
 
-            <div className='bg-[#007EAF] text-white px-20 py-20 space-y-14'>
-                <h1 className='font-Glory text-4xl'>Watch us</h1>
-                <h1 className='text-7xl font-bold'>Introducing: A new eharmony experience
+            <div className='bg-[#007EAF] text-white md:px-20 md:py-20 md:space-y-14 p-8 space-y-8'>
+                <h1 className='font-Glory md:text-4xl text-2xl'>Watch us</h1>
+                <h1 className='md:text-7xl text-3xl font-bold'>Introducing: A new eharmony experience
                 </h1>
                 <p className='text-xl'>Your search for a great relationship has never been easier with <br /> groundbreaking overhaul of the eharmony you know and trust.
                 </p>
@@ -221,11 +222,11 @@ const Home = () => {
 
             {/* Footer  */}
 
-            <div className='bg-[#2A2A2A] px-14 py-5 text-white space-y-10 text-lg'>
+            <div className='bg-[#2A2A2A] md:px-14 md:py-5 p-8 text-white space-y-10 text-lg'>
                 <div>
                     <img src={Logo} alt="" />
                 </div>
-                <div className='flex gap-60 '>
+                <div className='grid md:grid-cols-4 grid-cols-2 md:gap-60 gap-12'>
                     <div className='space-y-2'>
                         <h1 className='text-center'>Follow us on</h1>
                         <div className='flex gap-5 text-2xl'>
@@ -268,7 +269,7 @@ const Home = () => {
                 <div className='rounded-full bg-[#FFFFFF80] h-1 '></div>
                 <div className='space-y-10'>
                     <h1 className='text-2xl'>Get the app</h1>
-                    <div className='space-x-5'>
+                    <div className='md:space-x-5'>
                         <button className='rounded-2xl bg-[#FFFFFF] border-2 border-[#0000004D] text-black text-lg p-2'>
 
                             <div className='flex items-center gap-7  '>
